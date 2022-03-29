73eb6c007a6a2e32c5cf8d09121a7b1f6b71c490
Fuad2005 <fuadqasimov000@gmail.com>
2022-03-29 13:48:06 +0400

    homework8

---
+++ main.js
@@ -0,0 +1,95 @@
+// // 1)
+
+// const items = ['kitab', 'defter', 'qelem', 'pozan']
+// items.splice(2,1,'juran','karandas')
+// console.log(items)
+
+
+// 2)
+
+// const numbers = [2, 5, 32, 12, 6]
+// function oddEven (element){
+//     if(element%2==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// let checkEven = numbers.every(oddEven)
+// console.log(checkEven)
+
+// let min= Math.min(2,5,32,12,6)
+// let minEven = oddEven(min)
+// console.log(minEven)
+
+
+
+
+
+
+// 3)
+
+// const numbers = [-23, 5, 34, 3, -3, 0]
+
+// const positive = numbers.filter(function(num){return num>0})
+// console.log(positive)
+
+// const multiplied = positive.map(function(num){return num*2})
+// console.log(multiplied)
+
+
+// const maxToMin = multiplied.sort(function(a,b){ return b-a})
+// console.log(maxToMin)
+
+
+
+// 4)
+
+
+// let btn = 'save'
+
+// switch(btn){
+//     case 'save':
+//         btn= 'Yadda Saxlandi';
+//         break;
+//     case 'cancel':
+//         btn= 'Cixildi';
+//         break;
+//     case 'update':
+//         btn= 'Dəyişikliklər Yadda Saxlandı'  ;
+//         break;
+//     default:
+//         btn= 'Qebul Edilmedi';
+//         break;
+
+// }
+// console.log(btn)
+
+
+
+// 5)
+
+// const numbers= [4, 6, 3, 2, 5, 7]
+
+
+// const oddEven= numbers.map(function (element){
+//     if(element%2===0){
+//         return 'cut'
+//     }
+//     else{
+//         return 'tek'
+//     }
+// })
+// console.log(oddEven)
+
+
+
+
+
+
+
+
+
+
